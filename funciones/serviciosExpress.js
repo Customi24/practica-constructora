a770011058626b98dcbc62d0741bc531cf64b03d
Customi24 <mmorales152@alumnos.uaq.mx>
2026-05-06 11:26:03 -0600

    correccion de errores

--- funciones/serviciosExpress.js
+++ funciones/serviciosExpress.js
@@ -5,7 +5,7 @@ const serviciosExpress = [
         nombre: "Cimentacion Profunda",
         categoria: "Estructura",
         imagen: "img/serviciosExpress/cimentacion.jpg",
-        descripcicion: "Evaluacion tecnica y refuerzo estructural para grietas criticas en la base de tu construccion",
+        descripcion: "Evaluacion tecnica y refuerzo estructural para grietas criticas en la base de tu construccion",
         tiempo: "2-5 dias",
         precio: "Desde $5,000 MXN"
     },
@@ -14,7 +14,7 @@ const serviciosExpress = [
         nombre: "Remodelacion de interiores",
         categoria: "Acabados",
         imagen: "img/serviciosExpress/remodelacion.jpg",
-        descripcicion: "Transforma tus espacios con acabdos modernos y funcionales, adaptados a tu estilo de vida",
+        descripcion: "Transforma tus espacios con acabdos modernos y funcionales, adaptados a tu estilo de vida",
         tiempo: "3-7 dias",
         precio: "Desde $10,000 MXN"
     },
@@ -23,7 +23,7 @@ const serviciosExpress = [
         nombre: "Diseño de planos 3D",
         categoria: "Arquitectura",
         imagen: "img/serviciosExpress/planos.jpg",
-        descripcicion: "Creacion de planos arquitectonicos en 3D para visualizar tu proyecto antes de construirlo",
+        descripcion: "Creacion de planos arquitectonicos en 3D para visualizar tu proyecto antes de construirlo",
         tiempo: "5-10 dias",
         precio: "Desde $8,000 MXN"
     },
@@ -32,7 +32,7 @@ const serviciosExpress = [
         nombre: "Instalacion Electrica",
         categoria: "Instalaciones",
         imagen: "img/serviciosExpress/electrica.jpg",
-        descripcicion: "Instalacion electrica segura y eficiente para tu hogar o negocio, cumpliendo con los estandares de calidad",
+        descripcion: "Instalacion electrica segura y eficiente para tu hogar o negocio, cumpliendo con los estandares de calidad",
         tiempo: "1-3 dias",
         precio: "Desde $5,000 MXN"
     },
@@ -41,7 +41,7 @@ const serviciosExpress = [
         nombre: "Pintura y acabados",
         categoria: "Acabados",
         imagen: "img/serviciosExpress/pintura.jpg",
-        descripcicion: "Servicio de pintura profesional para interiores y exteriores, con acabados duraderos y de alta calidad",
+        descripcion: "Servicio de pintura profesional para interiores y exteriores, con acabados duraderos y de alta calidad",
         tiempo: "2-4 dias",
         precio: "Desde $3,000 MXN"
     },
@@ -50,7 +50,7 @@ const serviciosExpress = [
         nombre: "Reparacion de techos",
         categoria: "Estructura",
         imagen: "img/serviciosExpress/techos.jpg",
-        descripcicion: "Reparacion y mantenimiento de techos para garantizar su integridad estructural y resistencia",
+        descripcion: "Reparacion y mantenimiento de techos para garantizar su integridad estructural y resistencia",
         tiempo: "1-3 dias",
         precio: "Desde $7,000 MXN"
     },
